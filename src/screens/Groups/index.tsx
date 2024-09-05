7b16da808019f56862330abc68c26f748bb0da35
Gabriel Andrade <gabrielandrade248@gmail.com>
2024-09-05 19:36:38 -0300

    new header and new groupcard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
+
 import { Container } from './styles';
 
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { FlatList } from 'react-native';
+
 export function Groups() {
+  const [ groups, setGroups ] = useState<string[]>(['Rocketseat', 'Galera do trabalho', 'Galera do lolzin']);
+
   return (
     <Container>
-  
+      <Header/>
+
+      <Highlight
+        title='Turmas'
+        subtitle='jogue com a sua turma'
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+      />
     </Container>
   );
 }
