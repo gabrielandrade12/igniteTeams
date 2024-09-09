c868d2e4cd7f13faaaf7e6d169ae01c0be70b1ce
Gabriel Andrade <gabrielandrade248@gmail.com>
2024-09-09 11:38:12 -0300

    finished interface Groups create interface NewGroups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 import { Container } from './styles';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
-import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';  
 
 export function Groups() {
   const [ groups, setGroups ] = useState<string[]>(['Rocketseat', 'Galera do trabalho', 'Galera do lolzin']);
@@ -27,6 +29,16 @@ export function Groups() {
             title={item}
           />
         )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message='Que tal cadastrar a primeira turma?'
+          />
+        )}
+      />
+
+      <Button 
+        title='Criar nova turma'
       />
     </Container>
   );
