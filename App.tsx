336887cfd96304e4705b407342655f2283364c8b
Gabriel Andrade <gabrielandrade248@gmail.com>
2024-09-09 19:02:18 -0300

    created Players interface

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 
 import { Loading } from '@components/Loading';
 
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       /> 
-      { loaded ? <NewGroup/> : <Loading/> } 
+      { loaded ? <Players/> : <Loading/> } 
     </ThemeProvider>
   );
 }
