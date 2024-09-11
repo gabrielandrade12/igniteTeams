9dc3e3d4b1d148122a7fab648bdb949e50a87094
Gabriel Andrade <gabrielandrade248@gmail.com>
2024-09-11 11:56:24 -0300

    armazenamento e carregamento de dados localmente

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,8 @@ import { useNavigation } from "@react-navigation/native";
 
 import { Container, Content, Icon } from "./styles";
 
+import { groupCreate } from "@storage/group/groupCreate";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
@@ -13,8 +15,13 @@ export function NewGroup() {
 
     const navigation = useNavigation()
 
-    function handleNew(){
-        navigation.navigate('players', { group });
+    async function handleNew(){
+        try {
+            await groupCreate(group);
+            navigation.navigate('players', { group });
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return(
